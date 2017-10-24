92c4271d1507a59f05ad06214e066d3e947e38fb
Sam Delacruz <sam.delacruz@landinsight.io>
2017-10-24 17:57:57 +0100

    Fix ffmpeg thumbnail generation

--- filepreview.js
+++ filepreview.js
@@ -84,6 +84,7 @@ module.exports = {
           if (options.width > 0 && options.height > 0) {
             ffmpegArgs.splice(8, 0, '-vf' , 'scale='+ options.width + ':' + '-1');
           }
+          console.log('ffmpeg ' + ffmpegArgs.join(' '))
           child_process.execFile('ffmpeg', ffmpegArgs, function(error) {
             if (input_original.indexOf("http://") == 0 || input_original.indexOf("https://") == 0) {
               fs.unlinkSync(input);
